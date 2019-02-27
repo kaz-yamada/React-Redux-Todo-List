c44731475364aaf72373a4ab6f89d113104f70db
kaz-yamada <kaz.yamada@outlook.com>
2019-02-27 23:02:32 +1100

    Updates

---
+++ precache-manifest.144806ea3faf009f078c3f8f9b765520.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c578d6d621505e58183d",
+    "url": "/React-Redux-Todo-List/static/js/runtime~main.c578d6d6.js"
+  },
+  {
+    "revision": "0163f9a7cfb90bdca4c4",
+    "url": "/React-Redux-Todo-List/static/js/main.0163f9a7.chunk.js"
+  },
+  {
+    "revision": "eff5a89a41a883c111c4",
+    "url": "/React-Redux-Todo-List/static/js/2.eff5a89a.chunk.js"
+  },
+  {
+    "revision": "0163f9a7cfb90bdca4c4",
+    "url": "/React-Redux-Todo-List/static/css/main.d9a8977c.chunk.css"
+  },
+  {
+    "revision": "756d4994035c3e4f79812b274542158e",
+    "url": "/React-Redux-Todo-List/index.html"
+  }
+];
